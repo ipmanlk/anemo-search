46a66fb439fc518c7f424dd567b19aa3ee55e3ac
ipmanlk <io@navinda.xyz>
2020-12-30 17:11:15 +0530

    change: use int. observer for infinite scroll

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { Ref } from "react";
 import "./Card.css";
 import { Anime } from "../../types";
 
 interface IProps {
 	anime: Anime;
 	handleCardClick(id: number): void;
+	lastCardRef?: Ref<any>;
 }
 
 export function Card(props: IProps) {
 	const { anime, handleCardClick } = props;
 	return (
-		<div className="card">
+		<div className="card" ref={props.lastCardRef}>
 			<img
 				className="card-cover"
 				src={anime.picture}
--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useRef } from "react";
 import "./CardList.css";
 import { Card } from "../Card/Card";
 import { AnimeList } from "../../types";
@@ -10,26 +10,37 @@ interface IProps {
 }
 
 export function CardList(props: IProps) {
+	const observer = useRef<any>();
+
+	const lastCardRef = useCallback((node) => {
+		if (observer.current) observer.current.disconnect();
+		observer.current = new IntersectionObserver((entries) => {
+			if (entries[0].isIntersecting) {
+				props.handleCardListScrollToBottom();
+			}
+		});
+		if (node) observer.current.observe(node);
+	}, []);
+
 	return (
 		<div className="card-list">
-			<div
-				className="card-list-container"
-				onScroll={(e: any) => {
-					const element = e.target;
-					const visibleHeight = element.scrollTop;
-					const fullHeight = element.scrollHeight - element.offsetHeight;
-					if (visibleHeight == fullHeight) {
-						props.handleCardListScrollToBottom();
-					}
-				}}
-			>
-				{props.animeList.map((anime, index) => (
-					<Card
-						key={index}
-						anime={anime}
-						handleCardClick={props.handleCardClick}
-					/>
-				))}
+			<div className="card-list-container">
+				{props.animeList.map((anime, index) =>
+					props.animeList.length === index + 1 ? (
+						<Card
+							key={index}
+							lastCardRef={lastCardRef}
+							anime={anime}
+							handleCardClick={props.handleCardClick}
+						/>
+					) : (
+						<Card
+							key={index}
+							anime={anime}
+							handleCardClick={props.handleCardClick}
+						/>
+					)
+				)}
 			</div>
 		</div>
 	);
